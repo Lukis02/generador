55b845cddb02971280ac16e785cdc3e9ba7f351b
Lukis02 <74176312+Lukis02@users.noreply.github.com>
2021-02-12 00:27:31 +0100

    Add files via upload

---
+++ generador.java
@@ -0,0 +1,51 @@
+import javax.print.attribute.standard.JobPrioritySupported;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class generador extends JFrame {
+    public static String minusculas = "abcdefghijklmnñopqrstuvwxyz";
+    public static String MAYUSCULAS = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+    public static String numeros = "1234567890";
+    public static String ASCII = "()!·$%&/=¿?/*-+";
+    public generador() {
+        super("Generador de contraseñas");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(3, 1, 50, 50));
+        p1.add(new JTextField());
+        JPanel p2 = new JPanel();
+        p2.setLayout(new GridLayout(1,2));
+        JPanel p3 = new JPanel();
+        p3.setLayout(new GridLayout(4,1));
+        p3.add(new JCheckBox("Mayúsculas (ABCDEFGHIJKLMNÑOPQRSTUVWXYZ)"));
+        p3.add(new JCheckBox("Minúsculas (abcdefghijklmnñopqrstuvwxyz)"));
+        p3.add(new JCheckBox("números(1234567890)"));
+        p3.add(new JCheckBox("caracteres ASCII (!·$%&/=¿?/*-+)"));
+        p2.add(p3);
+        p2.add(new JSpinner());
+        JPanel p4 = new JPanel();
+        p4.setLayout(new GridLayout(1,2));
+        p4.add(new JButton("reiniciar"));
+        p4.add(new JButton("aceptar"));
+        p1.add(p2);
+        p1.add(p4);
+add(p1);
+        setSize(800,600);
+        setVisible(true);
+        }
+
+        public static void main(String[] args) {
+            generador gn= new generador();
+
+        }
+        private class ListenerBoton implements ActionListener {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+            }
+        }}
